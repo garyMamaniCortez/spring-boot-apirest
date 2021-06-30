3026e57f20694238c63f966de51dc91a0e4272f7
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-06-30 13:07:31 -0400

    initial commit

---
+++ src/test/java/com/curso/springboot/apirest/springbootapirest/SpringBootApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.apirest.springbootapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
