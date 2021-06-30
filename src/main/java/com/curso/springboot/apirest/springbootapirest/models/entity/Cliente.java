d5521afe0f802eec3771d0e4af811282243d22e9
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-06-30 19:18:02 -0400

    feat: create entity Cleinte

---
+++ src/main/java/com/curso/springboot/apirest/springbootapirest/models/entity/Cliente.java
@@ -0,0 +1,73 @@
+package com.curso.springboot.apirest.springbootapirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table( name="clientes" )
+public class Cliente implements Serializable{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    //si no se hara ninguna restriccion, o cambiara el nombre
+    //de la columna no es necesario poner @Column
+    //esto aplica tambien a @Table
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @Column(name="create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getApellido(){
+        return apellido;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public Date getCreateAt(){
+        return createAt;
+    }
+
+    public void setId(Long id){
+        this.id=id;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public void setApellido(String apellido){
+        this.apellido=apellido;
+    }
+
+    public void setEmail(String email){
+        this.email=email;
+    }
+    
+    public void setCreateAt(Date createAt){
+        this.createAt=createAt;
+    }
+
+    private static final long serialVersionUID= 1L;
+}
