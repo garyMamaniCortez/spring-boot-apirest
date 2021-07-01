31eae28e72104eac5d61a8cd0f884d98bf2a5db4
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-06-30 21:07:59 -0400

    fix: modified to service

--- src/main/java/com/curso/springboot/apirest/springbootapirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/curso/springboot/apirest/springbootapirest/models/services/ClienteServiceImpl.java
@@ -1,16 +1,20 @@
 package com.curso.springboot.apirest.springbootapirest.models.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import com.curso.springboot.apirest.springbootapirest.models.dao.IClienteDAO;
 import com.curso.springboot.apirest.springbootapirest.models.entity.Cliente;
 import java.util.List;
 
+@Service
 public class ClienteServiceImpl implements IClienteService{
     
     @Autowired
     private IClienteDAO clienteDao;
     @Override
+    @Transactional(readOnly=true)
     public List<Cliente> findAll(){
-        return null;
+        return (List<Cliente>) clienteDao.findAll();
     }
 }
