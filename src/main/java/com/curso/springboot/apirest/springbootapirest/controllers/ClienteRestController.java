11bf916341965d708fa88396f66653315ee5dc37
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-07-01 12:11:04 -0400

    feat: CrossOrigin libreries added

--- src/main/java/com/curso/springboot/apirest/springbootapirest/controllers/ClienteRestController.java
+++ src/main/java/com/curso/springboot/apirest/springbootapirest/controllers/ClienteRestController.java
@@ -1,14 +1,13 @@
 package com.curso.springboot.apirest.springbootapirest.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import com.curso.springboot.apirest.springbootapirest.models.entity.Cliente;
 import com.curso.springboot.apirest.springbootapirest.models.services.IClienteService;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
