7e30fa5980255f36f16b084d175c2ab28abf1f56
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-07-01 19:39:40 -0400

    feat: delete and update features for client

--- src/main/java/com/curso/springboot/apirest/springbootapirest/controllers/ClienteRestController.java
+++ src/main/java/com/curso/springboot/apirest/springbootapirest/controllers/ClienteRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
+
 import com.curso.springboot.apirest.springbootapirest.models.entity.Cliente;
 import com.curso.springboot.apirest.springbootapirest.models.services.IClienteService;
 
@@ -30,5 +31,21 @@ public class ClienteRestController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente){
         return clienteService.save(cliente);
+    }
+
+    @PutMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id){
+        Cliente clienteActual = clienteService.findById(id);
+        clienteActual.setNombre(cliente.getNombre());
+        clienteActual.setApellido(cliente.getApellido());
+        clienteActual.setEmail(cliente.getEmail());
+        return clienteService.save(clienteActual);
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        clienteService.delete(id);
     }
 }
